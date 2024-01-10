7f01cbc67bb73ba7e1222072c54da1a0bfd91151
Davide Moraschi <davidem@eurostrategy.net>
2024-01-10 16:41:47 +0100

    Update single_sending.py

--- examples/sender/single_sending.py
+++ examples/sender/single_sending.py
@@ -4,6 +4,28 @@
 # See the LICENSE file in the project root for more information.
 
 from zabbix_utils import Sender
+import boto3
+import json
+
+class flow_execution:
+    def __init__(self, id, executionstatus, catalogstatus, catalogerror):
+        self.id = id
+        self.executionstatus = executionstatus
+        self.catalogstatus = catalogstatus
+        self.catalogerror = catalogerror
+
+client = boto3.client('appflow')
+response = client.describe_flow_execution_records(
+    flowName='github-to-s3',
+    maxResults=1
+)
+
+flow_execution_result = flow_execution(
+    response.get('flowExecutions')[0].get('executionId'),
+    response.get('flowExecutions')[0].get('executionStatus'),
+    response.get('flowExecutions')[0].get('metadataCatalogDetails')[0].get('tableRegistrationOutput').get('status'),
+    response.get('flowExecutions')[0].get('metadataCatalogDetails')[0].get('tableRegistrationOutput').get('message')
+)
 
 # Zabbix server/proxy details for Sender
 ZABBIX_SERVER = {
@@ -16,7 +38,7 @@ sender = Sender(**ZABBIX_SERVER)
 
 # Send a value to a Zabbix server/proxy with specified parameters
 # Parameters: (host, key, value, clock, ns)
-responses = sender.send_value('zabbix-server', 'github-to-s3', 'PYTHON')
+responses = sender.send_value('zabbix-server', 'github-to-s3', json.dumps(flow_execution_result.__dict__))
 
 for node, resp in responses.items():
     # Check if the value sending was successful
