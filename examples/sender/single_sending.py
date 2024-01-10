58176d45e9f17ce6b21233c8ecfadc4366ad39ef
Davide Moraschi <davidem@eurostrategy.net>
2024-01-10 16:55:45 +0100

    testing signature

--- examples/sender/single_sending.py
+++ examples/sender/single_sending.py
@@ -4,10 +4,32 @@
 # See the LICENSE file in the project root for more information.
 
 from zabbix_utils import Sender
+import boto3
+import json
+
+class flow_execution:
+    def __init__(self, id, executionstatus, catalogstatus, catalogerror):
+        self.id = id
+        self.executionstatus = executionstatus
+        self.catalogstatus = catalogstatus
+        self.catalogerror = catalogerror
+
+client = boto3.client('appflow')
+response = client.describe_flow_execution_records(
+    flowName='github-to-s3',
+    maxResults=1
+)
+
+flow_execution_result = flow_execution(
+    response.get('flowExecutions')[0].get('executionId'),
+    response.get('flowExecutions')[0].get('executionStatus'),
+    response.get('flowExecutions')[0].get('metadataCatalogDetails')[0].get('tableRegistrationOutput').get('status'),
+    response.get('flowExecutions')[0].get('metadataCatalogDetails')[0].get('tableRegistrationOutput').get('message')
+)
 
 # Zabbix server/proxy details for Sender
 ZABBIX_SERVER = {
-    "server": "127.0.0.1",  # Zabbix server/proxy IP address or hostname
+    "server": "zabbix-server",  # Zabbix server/proxy IP address or hostname
     "port": 10051           # Zabbix server/proxy port for Sender
 }
 
@@ -16,7 +38,7 @@ sender = Sender(**ZABBIX_SERVER)
 
 # Send a value to a Zabbix server/proxy with specified parameters
 # Parameters: (host, key, value, clock, ns)
-responses = sender.send_value('host', 'item.key', 'value', 1695713666, 30)
+responses = sender.send_value('zabbix-server', 'github-to-s3', json.dumps(flow_execution_result.__dict__))
 
 for node, resp in responses.items():
     # Check if the value sending was successful
