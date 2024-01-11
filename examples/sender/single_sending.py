f136f012944d719b2cc54c6b3eb36d07a1f152ac
Davide Moraschi <davidem@eurostrategy.net>
2024-01-11 12:33:25 +0000

    adding timestamp

--- examples/sender/single_sending.py
+++ examples/sender/single_sending.py
@@ -5,18 +5,32 @@
 """Calls AWS AppFlow API to check for execution and returns the result to Zabbix"""
 
 import json
+import datetime
 import boto3
-
 from zabbix_utils import Sender
 
 
-class FlowExecution: # pylint: disable=too-few-public-methods
+def serialize_datetime(obj):
+    """Define a custom function to serialize datetime objects """
+    if isinstance(obj, datetime.datetime):
+        return obj.isoformat()
+    raise TypeError("Type not serializable")
+
+
+class FlowExecution:  # pylint: disable=too-few-public-methods
     """doc string"""
-    def __init__(self, executionid: int, executionstatus: str, catalogstatus: str, catalogerror: str):
+
+    def __init__(self, executionid: str, datapullendtime: datetime.datetime, executionstatus: str, recordsprocessed: int,
+                 catalogstatus: str = '', catalogerror: str = '', partitionstatus: str = '', partitiongerror: str = ''):
         self.executionid = executionid
+        self.datapullendtime = datapullendtime
         self.executionstatus = executionstatus
+        self.recordsprocessed = recordsprocessed
         self.catalogstatus = catalogstatus
         self.catalogerror = catalogerror
+        self.partitionstatus = partitionstatus
+        self.partitiongerror = partitiongerror
+
 
 client = boto3.client('appflow')
 response = client.describe_flow_execution_records(
@@ -25,25 +39,32 @@ response = client.describe_flow_execution_records(
 )
 
 flow_execution_result = FlowExecution(
-    executionid=response.get('flowExecutions')[0].get('executionId'),
-    executionstatus=response.get('flowExecutions')[0].get('executionStatus'),
-    catalogstatus=response.get('flowExecutions')[0].get('metadataCatalogDetails')[0].get('tableRegistrationOutput').get('status'),
-    catalogerror=response.get('flowExecutions')[0].get('metadataCatalogDetails')[0].get('tableRegistrationOutput').get('message')
+    executionid=response['flowExecutions'][0]['executionId'],
+    datapullendtime=response['flowExecutions'][0]['dataPullEndTime'],
+    executionstatus=response['flowExecutions'][0]['executionStatus'],
+    recordsprocessed=response['flowExecutions'][0]['executionResult']['recordsProcessed']
 )
 
+if 'metadataCatalogDetails' in response['flowExecutions'][0]:
+    flow_execution_result.catalogstatus = response['flowExecutions'][0]['metadataCatalogDetails'][0]['tableRegistrationOutput']['status']
+    flow_execution_result.catalogerror = response['flowExecutions'][0]['metadataCatalogDetails'][0]['tableRegistrationOutput']['message']
+    flow_execution_result.partitionstatus = response['flowExecutions'][0]['metadataCatalogDetails'][0]['partitionRegistrationOutput']['status']
+    flow_execution_result.partitiongerror = response['flowExecutions'][0]['metadataCatalogDetails'][0]['partitionRegistrationOutput']['message']
+
 # Zabbix server/proxy details for Sender
-ZABBIX_SERVER = {
-    "server": "zabbix-server",  # Zabbix server/proxy IP address or hostname
-    "port": 10051           # Zabbix server/proxy port for Sender
-}
+# ZABBIX_SERVER = {
+#     'server': 'zabbix-server',  # Zabbix server/proxy IP address or hostname
+#     'port': 10051               # Zabbix server/proxy port for Sender
+# }
 
 # Create an instance of the Sender class with the specified server details
-sender = Sender(**ZABBIX_SERVER)
+# sender = Sender(**ZABBIX_SERVER)
+sender = Sender('zabbix-server')
 
 # Send a value to a Zabbix server/proxy with specified parameters
 # Parameters: (host, key, value, clock, ns)
 responses = sender.send_value('zabbix-server', 'github-to-s3',
-                              json.dumps(flow_execution_result.__dict__))
+                              json.dumps(flow_execution_result.__dict__, default=serialize_datetime))
 
 for node, resp in responses.items():
     # Check if the value sending was successful
