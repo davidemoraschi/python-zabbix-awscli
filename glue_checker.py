2957d2a62571ccf2bbc334df70cac15f100ca8e8
Davide Moraschi <davidem@eurostrategy.net>
2024-01-16 15:01:44 +0100

    works

--- glue_checker.py
+++ glue_checker.py
@@ -8,7 +8,6 @@
 import sys
 import boto3
 from classes.GlueJobExecution import GlueJobExecution
-# from zabbix_utils import Sender as zabbix_sender
 
 
 def sort_jobexecution(gluejob):
@@ -18,46 +17,39 @@ def sort_jobexecution(gluejob):
 
 def main():
     '''Main function'''
-    joblist: list = []
-    jobname: str = str(sys.argv[1])
 
-    client = boto3.client('glue')
-    if jobname == '*':
-        response = client.get_jobs(MaxResults=1000)
-        for element in response['Jobs']:
-            joblist.append(element['Name'])
-    else:
-        joblist.append(jobname)
+    joblist: list = []
+
+    eu_west_1_session = boto3.Session(region_name=str(sys.argv[1]), profile_name=str(sys.argv[2]))
+    client = eu_west_1_session.client(service_name='glue')
+    response = client.get_jobs(MaxResults=1000)
+
+    for element in response['Jobs']:
+        joblist.append(element['Name'])
 
     gluejobexecution: GlueJobExecution = []
     for job in joblist:
         response = client.get_job_runs(JobName=job, MaxResults=1)
-        gluejobexecution.append(GlueJobExecution(
-            jobname=response['JobRuns'][0]['JobName'],
-            jobrunid=response['JobRuns'][0]['Id'],
-            jobrunstate=response['JobRuns'][0]['JobRunState'],
-            startedon=response['JobRuns'][0]['StartedOn'],
-            completedon=response['JobRuns'][0]['CompletedOn'],
-            executiontime=response['JobRuns'][0]['ExecutionTime'])
-        )
+        if response['JobRuns']:
+            gluejobexecution.append(GlueJobExecution(
+                jobname=response['JobRuns'][0]['JobName'],
+                jobrunid=response['JobRuns'][0]['Id'],
+                jobrunstate=response['JobRuns'][0]['JobRunState'],
+                startedon=response['JobRuns'][0]['StartedOn'],
+                completedon=response['JobRuns'][0]['CompletedOn'],
+                executiontime=response['JobRuns'][0]['ExecutionTime'])
+            )
+        else:
+            print(f"Empry result: {job}")
 
-    print(f'Glue Job                       - Last Status          Last Execution Date                Duration in Sec.')
+    print(f'Glue Job                                                - Last Status          Last Execution Date                Duration in Sec.')
     print(''.center(105, '-'))
 
     gluejobexecution.sort(reverse=True, key=sort_jobexecution)
 
     for gluejob in gluejobexecution:
-        print(f'{gluejob.jobname:30} - {gluejob.jobrunstate:20} {gluejob.completedon.isoformat():40} {gluejob.executiontime:10}')
+        print(f'{gluejob.jobname:55} - {gluejob.jobrunstate:20} {gluejob.completedon.isoformat():40} {gluejob.executiontime:10}')
     print(''.center(105, '-'))
-
-    # zabbixsender = zabbix_sender('zabbix-server')
-    # ret = zabbixsender.send_value('zabbix-server', jobname, gluejobexecution.tojson())
-
-    # for node, resp in ret.items():
-    #     if resp.failed == 0:
-    #         print(f"Value sent successfully to {node} in {resp.time}")
-    #     else:
-    #         print(f"Failed to send value to {node}")
 
 
 if __name__ == "__main__":
