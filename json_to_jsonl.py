a6737ec34db95e540ad9a35eda1116ab51f7e57a
Davide Moraschi <davidem@eurostrategy.net>
2024-01-25 09:32:51 +0000

    ok

--- json_to_jsonl.py
+++ json_to_jsonl.py
@@ -1,3 +1,23 @@
 import jq
+import json
 
-assert jq.compile(".+5").input_value(42).first() == 47
+# Opening JSON file 
+f = open('products_feed_2024-01-09-0402.json') 
+data = json.load(f)
+
+payload = jq.compile(".products[] as $parent | $parent.localizedAttributes.en_XD.name as $productname | $parent | {code, brand, $productname, salesAreaProperties}").input_value(data).text()
+
+#payload = iter(jq.compile(".products[] as $parent | $parent.localizedAttributes.en_XD.name as $productname | $parent | {code, brand, $productname, salesAreaProperties}").input_value(data))
+
+# Opening a file for writing
+file_object = open("output.txt", "w")
+file_object.writelines(payload)
+
+
+# for tuple in payload:
+#   #print(tuple)
+#   file_object.writelines(tuple)
+
+#print(payload)
+file_object.close()
+
