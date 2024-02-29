d3a4feb48dd03be30524d2a40532e1e8f6cad2a6
Davide Moraschi <davide.moraschi@straumann.com>
2024-02-29 09:51:46 +0100

    add error handling

--- workspace/stg-dlk-sbx-ds6-job-source-to-raw.py
+++ workspace/stg-dlk-sbx-ds6-job-source-to-raw.py
@@ -37,7 +37,7 @@ if '--JOB_NAME'     in sys.argv: params.append('JOB_NAME')
 args = getResolvedOptions(sys.argv, params)
 
 # Sets the report id and JOB_RUN_ID
-REPORT_ID   = args['reportid'] if 'reportid' in args else '10e865fe-75d1-49a5-bec4-b0db905023e4'
+REPORT_ID   = args['reportid'] if 'reportid' in args else '10e865fe-75d1-49a5-bec4-b0db905023e'
 JOB_NAME    = args['JOB_NAME'] if 'JOB_NAME' in args else 'stg-dlk-sbx-ds6-job-source-to-raw'
 JOB_RUN_ID  = int(time.time())
 LOG_DELAY = 50/1000
@@ -49,73 +49,96 @@ def fibonacci(n):
         return fibonacci(n-1) + fibonacci(n-2)
 
 def retrieve_access_token():
-    credentials = get_secret_credentials(AWS_REGION, AWS_SECRET_NAME)
-    token_url = 'https://clearcorrect.docebosaas.com/oauth2/token'
-    refresh_token = None  
+    '''Retrieves the access token from AWS Secrets Manager'''
 
-    token_manager = TokenManager(token_url, credentials, refresh_token)
-    access_token = token_manager.get_token()
-    return access_token
+    try:
+        credentials = get_secret_credentials(AWS_REGION, AWS_SECRET_NAME)
+        token_url = 'https://clearcorrect.docebosaas.com/oauth2/token'
+        refresh_token = None  
+        token_manager = TokenManager(token_url, credentials, refresh_token)
+        access_token = token_manager.get_token()
+        return access_token
+    except Exception as exc:
+        raise
 
 def create_export_report_csv(REPORT_ID):
-    token = retrieve_access_token()
-    headers = {'Authorization': f'Bearer {token}'}
-    url = f'https://clearcorrect.docebosaas.com/analytics/v1/reports/{REPORT_ID}/export/csv'
-    response = requests.get(url=url, headers=headers)
-    response.raise_for_status()                                         # Raise an exception for bad status codes
-    execution_id = response.json()['data']['executionId']
-    return execution_id
+    '''Calls the Docebo API to start a report extraction'''
+
+    try:
+        token = retrieve_access_token()
+        headers = {'Authorization': f'Bearer {token}'}
+        url = f'https://clearcorrect.docebosaas.com/analytics/v1/reports/{REPORT_ID}/export/csv'
+        response = requests.get(url=url, headers=headers)
+        response.raise_for_status()                                         # Raise an exception for bad status codes
+        execution_id = response.json()['data']['executionId']
+        return execution_id
+    except Exception as exc:
+        raise
 
 def download_export_report_csv(REPORT_ID, execution_id):
-    token = retrieve_access_token()
-    headers = {'Authorization': f'Bearer {token}'}
-    is_completed = False
-    fibonacci_index = 1
-    url = f'https://clearcorrect.docebosaas.com/analytics/v1/reports/{REPORT_ID}/exports/{execution_id}'
+    '''Calls the Docebo API to download the report zip file'''
 
-    while not is_completed:
-        sleep_time = fibonacci(fibonacci_index)                         # Get the Fibonacci number
-        fibonacci_index += 1                                            # Increment Fibonacci index for the next iteration
-        time.sleep(sleep_time)                                          # Sleep for the Fibonacci number of seconds
-        response = requests.get(url=url, headers=headers)               # Calls the endpoint to get report status
-        response.raise_for_status()                                     # Raise an exception for bad status codes
-        is_completed = (response.json()['data']['status']=='SUCCEEDED') # Check if finished
+    try:
+        token = retrieve_access_token()
+        headers = {'Authorization': f'Bearer {token}'}
+        is_completed = False
+        fibonacci_index = 1
+        url = f'https://clearcorrect.docebosaas.com/analytics/v1/reports/{REPORT_ID}/exports/{execution_id}'
 
-    url = f'https://clearcorrect.docebosaas.com/analytics/v1/reports/{REPORT_ID}/exports/{execution_id}/download'
-    response = requests.get(url=url, headers=headers)                   # Calls the endpoint to download report zip file
-    response.raise_for_status()                                         # Raise an exception for bad status codes
-    with zipfile.ZipFile(io.BytesIO(response.content), 'r') as zip_ref: # Unzips the content on the fly
-        zip_ref.extractall('./')                                        # Saves into current folder
-        return zip_ref.filelist
+        while not is_completed:
+            sleep_time = fibonacci(fibonacci_index)                         # Get the Fibonacci number
+            fibonacci_index += 1                                            # Increment Fibonacci index for the next iteration
+            time.sleep(sleep_time)                                          # Sleep for the Fibonacci number of seconds
+            response = requests.get(url=url, headers=headers)               # Calls the endpoint to get report status
+            response.raise_for_status()                                     # Raise an exception for bad status codes
+            is_completed = (response.json()['data']['status']=='SUCCEEDED') # Check if finished
+
+        url = f'https://clearcorrect.docebosaas.com/analytics/v1/reports/{REPORT_ID}/exports/{execution_id}/download'
+        response = requests.get(url=url, headers=headers)                   # Calls the endpoint to download report zip file
+        response.raise_for_status()                                         # Raise an exception for bad status codes
+        with zipfile.ZipFile(io.BytesIO(response.content), 'r') as zip_ref: # Unzips the content on the fly
+            zip_ref.extractall('./')                                        # Saves into current folder
+            return zip_ref.filelist
+    except Exception as exc:
+        raise
 
 def main():
     '''Guess what, I'm the main module...'''
 
-    # Mark the start of the Job execution for Monitoring with CloudWatch.
-    glue_job_logger.info('JOB_NAME.JOB_RUN_ID           : %s.%s | start-job', JOB_NAME, str(JOB_RUN_ID))
-    time.sleep(LOG_DELAY)
-    glue_job_logger.info('REPORT_ID                     : %s', REPORT_ID)
-    time.sleep(LOG_DELAY)
+    try:
+        # Mark the start of the Job execution for Monitoring with CloudWatch.
+        glue_job_logger.info('JOB_NAME.JOB_RUN_ID           : %s.%s | start-job', JOB_NAME, str(JOB_RUN_ID))
+        time.sleep(LOG_DELAY)
+        glue_job_logger.info('REPORT_ID                     : %s', REPORT_ID)
+        time.sleep(LOG_DELAY)
 
-    # Prints to log several useful info for debugging purposes.
-    log_environment(logger=glue_job_logger, environment=ENVIRONMENT, job_name=JOB_NAME, job_run_id=JOB_RUN_ID, args=ARGS)
+        # Prints to log several useful info for debugging purposes.
+        log_environment(logger=glue_job_logger, environment=ENVIRONMENT, job_name=JOB_NAME, job_run_id=JOB_RUN_ID, args=ARGS)
 
-    # Runs the report and creates the csv
-    execution_id = create_export_report_csv(REPORT_ID)
+        # Runs the report and creates the csv
+        execution_id = create_export_report_csv(REPORT_ID)
 
-    # Downloads the zipped CSV
-    filelist = download_export_report_csv(REPORT_ID, execution_id)
-    
-    # Uploads them to the raw bucket
-    s3client = boto3.client('s3')
-    for csvfile in filelist:
-        s3_filename = csvfile.filename
-        with open(s3_filename, 'r') as file:
-            csvcontent = file.read()
-        upload_json_gz(glue_job_logger, s3client, S3_BUCKET, S3_PATH + f'{str(JOB_RUN_ID)}-{s3_filename}.gz', csvcontent)
+        # Downloads the zipped CSV
+        filelist = download_export_report_csv(REPORT_ID, execution_id)
+        
+        # Uploads them to the raw bucket
+        s3client = boto3.client('s3')
+        for csvfile in filelist:
+            s3_filename = csvfile.filename
+            with open(s3_filename, 'r') as file:
+                csvcontent = file.read()
+            upload_json_gz(glue_job_logger, s3client, S3_BUCKET, S3_PATH + f'{str(JOB_RUN_ID)}-{s3_filename}.gz', csvcontent)
 
-    # Mark the end of the Job execution for Monitoring with CloudWatch.
-    glue_job_logger.info('JOB_NAME.JOB_RUN_ID           : %s.%s | end-job', JOB_NAME, str(JOB_RUN_ID))
+        # Mark the end of the Job execution for Monitoring with CloudWatch.
+        glue_job_logger.info('JOB_NAME.JOB_RUN_ID           : %s.%s | end-job', JOB_NAME, str(JOB_RUN_ID))
+
+    except Exception as exc:
+        # Log any exceptions that occur during the execution of the script
+        # glue_job_logger.error(f'{JOB_NAME}.{str(JOB_RUN_ID)}: An error occurred: {str(exc)}')
+        glue_job_logger.error('JOB_NAME.JOB_RUN_ID           : %s.%s | An error occurred: %s', JOB_NAME, str(JOB_RUN_ID), str(exc))
+        time.sleep(LOG_DELAY)
+        glue_job_logger.info('JOB_NAME.JOB_RUN_ID           : %s.%s | end-job', JOB_NAME, str(JOB_RUN_ID))
+        raise  # Re-raise the exception to halt the execution and propagate the error
 
 
 if __name__ == "__main__":
