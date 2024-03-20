a34cf4ed0823ba1ca5f5e310461b2863c7b942c7
Davide Moraschi <davide.moraschi@straumann.com>
2024-03-20 14:58:41 +0100

    ok

--- ds9-pipeline/sap_cdc_primer.py
+++ ds9-pipeline/sap_cdc_primer.py
@@ -17,11 +17,10 @@ def get_headers() -> Dict[str, str]:
     # token = retrieve_access_token()
     return {"Content-Type": "application/x-www-form-urlencoded"}
 
-
 headers: Dict[str, str] = get_headers()
 
 # Construct API URL
-url: str = f'https://accounts.eu1.gigya.com/accounts.search?apiKey=3_sUuYY3aPXsYTusEqibJXwme9HwNRxM-N3_Wx2Z4HV3X47ft6BZY-JzcA2kBMaPcy&userKey=ADwPPFXaaoAn&secret=2lm93KoFJBKPvrKfdc5qFx5oqcbx5TcX&query=SELECT%20UID%20FROM%20accounts%20limit%2010000&timeout=60000&context=awsglue&httpStatusCodes=true&openCursor=true'
+url: str = f'https://accounts.eu1.gigya.com/accounts.search?apiKey=3_sUuYY3aPXsYTusEqibJXwme9HwNRxM-N3_Wx2Z4HV3X47ft6BZY-JzcA2kBMaPcy&userKey=ADwPPFXaaoAn&secret=2lm93KoFJBKPvrKfdc5qFx5oqcbx5TcX&query=SELECT%20UID%20AS%20i%20FROM%20accounts%20limit%2010000&timeout=60000&context=awsglue&httpStatusCodes=true&openCursor=true'
 
 # Make API request
 response: requests.Response = requests.post(url=url, headers=headers)
