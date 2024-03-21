16580ef1d0d7ef71b3fb60ff8825ef1692619be1
Davide Moraschi <davide.moraschi@straumann.com>
2024-03-21 16:53:54 +0100

    started glue job

--- ds9-pipeline/artifacts/ds9/glue/code/get_audit_table.py
+++ ds9-pipeline/artifacts/ds9/glue/code/get_audit_table.py
@@ -1,21 +1,31 @@
 #!/usr/bin/python
 '''
-Testing the GSSDK
+Testing the SAP CDC API to get the audit logs
 '''
 
 import json
+import os
+import time
 import boto3
 from botocore.exceptions import ClientError
 from typing import Dict
-# from gzip_s3_and_json_py3 import upload_json_gz
-# from config import S3_DESTINATION_BUCKET, S3_DESTINATION_PATH
+from gzip_s3_and_json_py3 import upload_json_gz
+from config import S3_DESTINATION_BUCKET, S3_DESTINATION_PATH
 import requests
 import jq
 
+TIMEOUT = 60000
+ROWLIMIT = 10000
+JOB_RUN_ID: int = int(time.time())
+
 
 def get_secret() -> Dict[str, str]:
-    '''Retrieves the secret from AWS Secrets Manager'''
+    '''
+    Retrieves the secret from AWS Secrets Manager
 
+    Returns:
+        A dictionary containing the secret values.
+    '''
     secret_name: str = "stg/dlk/sapcdc/ds9/secrets"
     region_name: str = "eu-west-1"
 
@@ -35,14 +45,15 @@ def get_secret() -> Dict[str, str]:
 
 creds: Dict[str, str] = json.loads(s=get_secret())  # type: ignore
 BASEURL = creds['audit.search']['URL']              # type: ignore
-TIMEOUT = 60000
-CONTEXT = 'awsglue'
-ROWLIMIT = 10
 
 
 def get_headers() -> Dict[str, str]:
-    '''Returns the headers for the API requests'''
+    '''
+    Returns the headers for the API requests
 
+    Returns:
+        A dictionary containing the headers.
+    '''
     return {
         "Content-Type": "application/x-www-form-urlencoded",
         "Accept-Encoding": "deflate, gzip;q=1.0, *;q=0.5"
@@ -50,8 +61,12 @@ def get_headers() -> Dict[str, str]:
 
 
 def get_params() -> Dict[str, str]:
-    '''Returns the parameters for the API requests'''
+    '''
+    Returns the parameters for the API requests
 
+    Returns:
+        A dictionary containing the parameters.
+    '''
     params: dict = {}
     params["secret"] = creds['secret']
     params["userKey"] = creds['userKey']
@@ -62,12 +77,15 @@ def get_params() -> Dict[str, str]:
 
 
 def main():
-    '''Main function that executes the script.'''
-
+    '''
+    Main function that executes the script.
+    '''
     nextCursorId: str = None                                # type: ignore
     headers: Dict[str, str] = get_headers()
     params: Dict[str, str] = get_params()
     objectscount: int = 0
+    if os.path.exists('payload.json'):
+        os.remove('payload.json')
 
     while True:
 
@@ -81,7 +99,7 @@ def main():
         else:
             params['openCursor'] = True                    # type: ignore
             params['query'] = f"SELECT * FROM auditLog \
-                                WHERE (@timestamp >= '2024-03-20T00:00:00.000Z' and @timestamp < '2024-03-21T00:00:00.000Z')\
+                                WHERE (@timestamp >= '2024-03-20T00:00:00.000Z' and @timestamp < '2024-03-20T06:00:00.000Z')\
                                 LIMIT {ROWLIMIT}"
 
         # Make API request
@@ -97,17 +115,28 @@ def main():
         with open('payload.json', 'a') as file:
             jsonl = jq.compile(".[]").input_value(payload['results']).text()
             file.write(jsonl)
+            
+            # check if the end of the result
+            if payload['nextCursorId'] is None:
+                # isSucceeded = True
+                # saveJSON.write(f"{json.dumps(tempOutput)[1:]}")
+                break
+            else:
+                file.write('\n')
+                # remove the {} and add a comma
+                # saveJSON.write(f"{json.dumps(tempOutput)[1:-1]},")
+                nextCursorId = payload['nextCursorId']
+    
+    with open('payload.json', 'r') as file:
+        jsonl = file.read()
 
-        # check if the end of the result
-        if payload['nextCursorId'] is None:
-            # isSucceeded = True
-            # saveJSON.write(f"{json.dumps(tempOutput)[1:]}")
-            break
-        else:
-            # remove the {} and add a comma
-            # saveJSON.write(f"{json.dumps(tempOutput)[1:-1]},")
-            nextCursorId = payload['nextCursorId']
+    # Creates boto3 session/client
+    glue_job_session = boto3.Session()
+    s3_client = glue_job_session.client(service_name='s3')
 
+    # Uploads to S3 raw bucket/folder
+    upload_json_gz(s3client=s3_client, bucket=S3_DESTINATION_BUCKET,
+                   key=f'{S3_DESTINATION_PATH}{JOB_RUN_ID}.json.gz', obj=jsonl)
 
 if __name__ == "__main__":
     main()
--- ds9-pipeline/main.tf
+++ ds9-pipeline/main.tf
@@ -28,6 +28,7 @@ locals {
   kms_key_arn               = "arn:aws:kms:eu-west-1:816247855850:key/396cd8ff-4b3d-4b17-9df4-9449185fdd2e"
   ext_user_name             = "ds${var.datasource_number}_sap_cdp_ext_user"
   # ext_role_name           = "ds${var.datasource_number}_sap_cdp_ext_role"
+  sapcdc_api_secret_name   = "stg/dlk/sapcdc/ds9/secrets"
 }
 
 # ===========================================================================================================================
@@ -41,7 +42,8 @@ resource "aws_s3_bucket" "raw_bucket" {
 
 resource "aws_s3_bucket_policy" "raw_bucket_policy" {
   bucket = aws_s3_bucket.raw_bucket.id
-  policy  = templatefile("${path.module}/artifacts/ds${var.datasource_number}/glue/policies/bucket-policy.json", { resource-arn = aws_s3_bucket.raw_bucket.arn })
+  policy  = templatefile("${path.module}/artifacts/ds${var.datasource_number}/glue/policies/bucket-policy.json", 
+  { resource-arn = aws_s3_bucket.raw_bucket.arn })
 }
 
 resource "aws_s3_bucket_public_access_block" "raw_bucket_access_block" {
@@ -111,34 +113,35 @@ resource "aws_s3_bucket_lifecycle_configuration" "raw_bucket_lifecycle" {
 #   policy_arn  = aws_iam_policy.ext_user_policy.arn
 # }
 
-# resource "aws_iam_role" "raw_job_role" {
-#   name               = local.raw_role_name
-#   assume_role_policy = templatefile("${path.module}/artifacts/ds${var.datasource_number}/glue/policies/trust-policy.json", 
-#     { service = "glue.amazonaws.com" })
-# }
+resource "aws_iam_role" "raw_job_role" {
+  name               = local.raw_role_name
+  assume_role_policy = templatefile("${path.module}/artifacts/ds${var.datasource_number}/glue/policies/trust-policy.json", 
+    { service = "glue.amazonaws.com" })
+}
 
-# resource "aws_iam_policy" "raw_job_role_policy" {
-#   name = "${local.raw_role_name}_policy"
-#   policy = templatefile("${path.module}/artifacts/ds${var.datasource_number}/glue/policies/glue-role-policy.json", {  
-#     account_id          = data.aws_caller_identity.current.account_id
-#     region              = data.aws_region.current.name
-#     resource-arn        = aws_s3_bucket.raw_bucket.arn
-#     # ext-resource-arn  = "arn:aws:s3:::${local.ext_bucket_name}"
-#     raw_db_name         = local.raw_database_name
-#     refined_db_name     = local.refined_database_name
-#     failure_topic_name  = "ds${var.datasource_number}-failure-topic"
-#   kms_key_arn = local.kms_key_arn })
-# }
+resource "aws_iam_policy" "raw_job_role_policy" {
+  name = "${local.raw_role_name}_policy"
+  policy = templatefile("${path.module}/artifacts/ds${var.datasource_number}/glue/policies/glue-role-policy.json", {  
+    account_id              = data.aws_caller_identity.current.account_id
+    region                  = data.aws_region.current.name
+    resource-arn            = aws_s3_bucket.raw_bucket.arn
+    # ext-resource-arn      = "arn:aws:s3:::${local.ext_bucket_name}"
+    raw_db_name             = local.raw_database_name
+    refined_db_name         = local.refined_database_name
+    failure_topic_name      = "ds${var.datasource_number}-failure-topic"
+    sapcdc_api_secret_name  = local.sapcdc_api_secret_name
+  kms_key_arn = local.kms_key_arn })
+}
 
-# resource "aws_iam_role_policy_attachment" "raw_job_role_policy_attachment" {
-#   role       = aws_iam_role.raw_job_role.name
-#   policy_arn = aws_iam_policy.raw_job_role_policy.arn
-# }
+resource "aws_iam_role_policy_attachment" "raw_job_role_policy_attachment" {
+  role       = aws_iam_role.raw_job_role.name
+  policy_arn = aws_iam_policy.raw_job_role_policy.arn
+}
 
-# resource "aws_lakeformation_resource" "raw_data_location" {
-#   arn      = aws_s3_bucket.raw_bucket.arn
-#   role_arn = aws_iam_role.raw_job_role.arn
-# }
+resource "aws_lakeformation_resource" "raw_data_location" {
+  arn      = aws_s3_bucket.raw_bucket.arn
+  role_arn = aws_iam_role.raw_job_role.arn
+}
 
 resource "aws_glue_catalog_database" "raw_glue_database" {
   name         = local.raw_database_name
@@ -146,22 +149,22 @@ resource "aws_glue_catalog_database" "raw_glue_database" {
   location_uri = "s3://${local.raw_bucket_name}"
 }
 
-# resource "aws_lakeformation_permissions" "raw_database_permissions" {
-#   principal   = aws_iam_role.raw_job_role.arn
-#   permissions = ["CREATE_TABLE", "DESCRIBE"]
-#   database {
-#     name = aws_glue_catalog_database.raw_glue_database.name
-#   }
-# }
+resource "aws_lakeformation_permissions" "raw_database_permissions" {
+  principal   = aws_iam_role.raw_job_role.arn
+  permissions = ["CREATE_TABLE", "DESCRIBE"]
+  database {
+    name = aws_glue_catalog_database.raw_glue_database.name
+  }
+}
 
-# resource "aws_lakeformation_permissions" "raw_tables_permissions" {
-#   principal   = aws_iam_role.raw_job_role.arn
-#   permissions = ["ALL"]
-#   table {
-#     database_name = aws_glue_catalog_database.raw_glue_database.name
-#     wildcard      = true
-#   }
-# }
+resource "aws_lakeformation_permissions" "raw_tables_permissions" {
+  principal   = aws_iam_role.raw_job_role.arn
+  permissions = ["ALL"]
+  table {
+    database_name = aws_glue_catalog_database.raw_glue_database.name
+    wildcard      = true
+  }
+}
 
 # # resource "aws_glue_catalog_table" "page_views_jsonl_gzip" {
 # #   name                            = "page_views_jsonl_gzip"
@@ -257,64 +260,70 @@ resource "aws_glue_catalog_database" "raw_glue_database" {
 #   source_hash = filemd5("${path.module}/artifacts/ds${var.datasource_number}/glue/code/${local.raw_script_name}.py")
 # }
 
-# resource "aws_s3_object" "glue_job_config" {
-#   bucket      = local.artifacts_bucket_name
-#   key         = "artifacts/glue_job_${local.datasource}/code/config.py"
-#   source      = "${path.module}/artifacts/ds${var.datasource_number}/glue/code/config.py"
-#   source_hash = filemd5("${path.module}/artifacts/ds${var.datasource_number}/glue/code/config.py")
-# }
+resource "aws_s3_object" "raw_glue_job_script" {
+  bucket      = local.artifacts_bucket_name
+  key         = "artifacts/glue_job_${local.datasource}/code/get_audit_table.py"
+  source      = "${path.module}/artifacts/ds${var.datasource_number}/glue/code/get_audit_table.py"
+  source_hash = filemd5("${path.module}/artifacts/ds${var.datasource_number}/glue/code/get_audit_table.py")
+}
 
-# resource "aws_s3_object" "glue_job_common_functions" {
-#   bucket      = local.artifacts_bucket_name
-#   key         = "artifacts/glue_job_${local.datasource}/code/common_functions.py"
-#   source      = "${path.module}/artifacts/ds${var.datasource_number}/glue/code/common_functions.py"
-#   source_hash = filemd5("${path.module}/artifacts/ds${var.datasource_number}/glue/code/common_functions.py")
-# }
+resource "aws_s3_object" "glue_job_config" {
+  bucket      = local.artifacts_bucket_name
+  key         = "artifacts/glue_job_${local.datasource}/code/config.py"
+  source      = "${path.module}/artifacts/ds${var.datasource_number}/glue/code/config.py"
+  source_hash = filemd5("${path.module}/artifacts/ds${var.datasource_number}/glue/code/config.py")
+}
 
-# resource "aws_s3_object" "glue_job_gzip_s3_and_json" {
-#   bucket      = local.artifacts_bucket_name
-#   key         = "artifacts/glue_job_${local.datasource}/code/gzip_s3_and_json_py3.py"
-#   source      = "${path.module}/artifacts/ds${var.datasource_number}/glue/code/gzip_s3_and_json_py3.py"
-#   source_hash = filemd5("${path.module}/artifacts/ds${var.datasource_number}/glue/code/gzip_s3_and_json_py3.py")
-# }
+resource "aws_s3_object" "glue_job_common_functions" {
+  bucket      = local.artifacts_bucket_name
+  key         = "artifacts/glue_job_${local.datasource}/code/common_functions.py"
+  source      = "${path.module}/artifacts/ds${var.datasource_number}/glue/code/common_functions.py"
+  source_hash = filemd5("${path.module}/artifacts/ds${var.datasource_number}/glue/code/common_functions.py")
+}
 
-# resource "aws_glue_job" "raw_glue_job" {
-#   name                            = local.raw_script_name
-#   description                     = "Part 1 of SAP CDP. Strips the outer array from the JSON and uploads it to the stg-dlk-sbx-ds-11-raw bucket."
-#   role_arn                        = aws_iam_role.raw_job_role.arn
-#   timeout                         = 15
-#   max_capacity                    = 0.0625
-#   command {
-#     name                          = "pythonshell"
-#     script_location               = "s3://${local.artifacts_bucket_name}/artifacts/glue_job_${local.datasource}/code/${local.raw_script_name}.py"
-#     python_version                = 3.9
-#   }
-#   security_configuration          = "dlk-glue-sec-config"
-#   default_arguments = {
-#     "library-set"                 = "analytics"
-#     "--additional-python-modules" = "paramiko,jq,tabulate"
-#     # "--bucket_name"             = "${var.raw_bucket_name}"
-#     # "--enable-continuous-cloudwatch-log"  = false
-#     "--enable-glue-datacatalog"   = true
-#     # "--enable-metrics"          = false
-#     # "--enable-spark-ui"         = false
-#     # "--enable-job-insights"     = false
-#     "--environment"               = "sbx"
-#     "--extra-files"               = "s3://stg-dlk-sbx-code-artifacts/artifacts/glue_job_${local.datasource}/code/config.py,s3://stg-dlk-sbx-code-artifacts/artifacts/glue_job_${local.datasource}/code/common_functions.py,s3://stg-dlk-sbx-code-artifacts/artifacts/glue_job_${local.datasource}/code/gzip_s3_and_json_py3.py"
-#     # "--region"                  = "eu-west-1"
-#     "--job-bookmark-option"       = "job-bookmark-disable"
-#     "--job-language"              = "python"
-#     "--WORKFLOW_NAME"             = "no_workflow"
-#     "--WORKFLOW_RUN_ID"           = "0"
-#     "--EVENT_TYPES"               = "[\"PAGE_VIEW\",\"BIOMATERIAL_STATE_UPDATE\",\"BIOMATERIAL_MILESTONE_UPDATE\"]"
-#     "--TempDir"                   = "s3://stg-dlk-sbx-glue-job-temporary-files/temporary/"
-#   }
-#   tags = {
-#     Author                        = "davide.moraschi@toptal.com"
-#     # ManagedBy                   = "Terraform"
-#     Project                       = "stg-dlk"
-#   }
-# }
+resource "aws_s3_object" "glue_job_gzip_s3_and_json" {
+  bucket      = local.artifacts_bucket_name
+  key         = "artifacts/glue_job_${local.datasource}/code/gzip_s3_and_json_py3.py"
+  source      = "${path.module}/artifacts/ds${var.datasource_number}/glue/code/gzip_s3_and_json_py3.py"
+  source_hash = filemd5("${path.module}/artifacts/ds${var.datasource_number}/glue/code/gzip_s3_and_json_py3.py")
+}
+
+resource "aws_glue_job" "raw_glue_job" {
+  name                            = local.raw_script_name
+  description                     = "Part 1 of SAP CDC. Merge all JSON files and uploads one to the stg-dlk-sbx-ds-9-raw bucket."
+  role_arn                        = aws_iam_role.raw_job_role.arn
+  timeout                         = 15
+  max_capacity                    = 0.0625
+  command {
+    name                          = "pythonshell"
+    script_location               = "s3://${local.artifacts_bucket_name}/artifacts/glue_job_${local.datasource}/code/get_audit_table.py"
+    python_version                = 3.9
+  }
+  security_configuration          = "dlk-glue-sec-config"
+  default_arguments = {
+    "library-set"                 = "analytics"
+    "--additional-python-modules" = "paramiko,jq,tabulate"
+    # "--bucket_name"             = "${var.raw_bucket_name}"
+    # "--enable-continuous-cloudwatch-log"  = false
+    "--enable-glue-datacatalog"   = true
+    # "--enable-metrics"          = false
+    # "--enable-spark-ui"         = false
+    # "--enable-job-insights"     = false
+    "--environment"               = "sbx"
+    "--extra-files"               = "s3://stg-dlk-sbx-code-artifacts/artifacts/glue_job_${local.datasource}/code/config.py,s3://stg-dlk-sbx-code-artifacts/artifacts/glue_job_${local.datasource}/code/common_functions.py,s3://stg-dlk-sbx-code-artifacts/artifacts/glue_job_${local.datasource}/code/gzip_s3_and_json_py3.py"
+    # "--region"                  = "eu-west-1"
+    "--job-bookmark-option"       = "job-bookmark-disable"
+    "--job-language"              = "python"
+    "--WORKFLOW_NAME"             = "no_workflow"
+    "--WORKFLOW_RUN_ID"           = "0"
+    "--TempDir"                   = "s3://stg-dlk-sbx-glue-job-temporary-files/temporary/"
+  }
+  tags = {
+    Author                        = "davide.moraschi@toptal.com"
+    # ManagedBy                   = "Terraform"
+    Project                       = "stg-dlk"
+  }
+}
 
 # # Workflow may not be the best approach for running the same job multiple times with parameters,
 # # but Step Funcions are a bad beast to debug. Eventually it is better to use a loop inside the Glue Job
