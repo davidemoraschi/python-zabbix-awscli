eb5895f7f6533d4d904a17d92d0a5eea35fa3d58
Davide Moraschi <davide.moraschi@straumann.com>
2024-04-26 14:51:09 +0200

    ok

--- ds14-pipeline/artifacts/ds14/glue/code/common_functions.py
+++ ds14-pipeline/artifacts/ds14/glue/code/common_functions.py
@@ -11,19 +11,44 @@ import sys
 import logging.config
 import logging
 import time
-import logging
 from typing import Dict, Any
+from functools import lru_cache
+from config import AWS_REGION, AWS_BUCKET, AWS_ATHENA_DATABASE, AWS_ATHENA_OUPUT, AWS_ATHENA_LOG_TABLE, S3_BUCKET, SQL_QUERIES_BUCKET, SQL_QUERIES_FOLDER, S3_PATH, SNS_FAILURE_TOPIC  # pylint: disable=line-too-long
 import boto3
 import awswrangler as wr
 from tabulate import tabulate
-from awsglue.utils import getResolvedOptions  # type: ignore
-from config import AWS_REGION, AWS_BUCKET,  AWS_ATHENA_DATABASE, AWS_ATHENA_OUPUT, SQL_QUERIES_BUCKET, SQL_QUERIES_FOLDER, S3_DESTINATION_BUCKET, S3_DESTINATION_PATH, SNS_FAILURE_TOPIC
-from functools import lru_cache
+from awsglue.utils import getResolvedOptions  # type: ignore # pylint: disable=import-error
+
+def process_arguments(options) -> Dict[str, str]:
+    '''These parameters will be populated by the trigger job paramters in Workflow or default to the Job configuration parameters'''
+
+    args: dict[str, str] = getResolvedOptions(args=sys.argv, options=options)
+    return args
 
 LOG_DELAY = 50/1000
 
-class InfoDebugFilter(logging.Filter):
-    '''I don't know what to write here, it's just a class that I copied from somewhere.'''
+# Process command line arguments
+args: Dict[str, str] = process_arguments(options=['WORKFLOW_NAME', 'WORKFLOW_RUN_ID', 'environment'])
+
+# Print arguments for debugging
+print(tabulate(tabular_data=args.items(), headers=['args.keys()', 'args.values()'], tablefmt='psql', showindex=False))
+
+# Parse report IDs, workflow name, EVENT_TYPES, and job name from arguments
+WORKFLOW_NAME: str = args['WORKFLOW_NAME']
+WORKFLOW_RUN_ID: str = args['WORKFLOW_RUN_ID']
+ENVIRONMENT: str = args['environment']
+JOB_NAME: str = args['JOB_NAME'] if 'JOB_NAME' in args else f'stg-dlk-{ENVIRONMENT}-ds6-job-raw-to-refined'
+JOB_RUN_ID: int = int(time.time())
+
+aws_bucket = AWS_BUCKET.replace('${var.env}',ENVIRONMENT)
+aws_athena_database = AWS_ATHENA_DATABASE.replace('${var.env}',ENVIRONMENT)
+s3_bucket = S3_BUCKET.replace('${var.env}',ENVIRONMENT)
+s3_path = S3_PATH.replace('${var.env}',ENVIRONMENT)
+sql_queries_bucket = SQL_QUERIES_BUCKET.replace('${var.env}',ENVIRONMENT)
+aws_athena_output = AWS_ATHENA_OUPUT.replace('${var.env}',ENVIRONMENT)
+
+class InfoDebugFilter(logging.Filter):  # pylint: disable=too-few-public-methods
+    '''Class InfoDebugFilter.'''
 
     def filter(self, record):
         """Filter debug and info messages."""
@@ -91,16 +116,86 @@ def default_logging_config(level: str = "INFO", formatter_name: str = "detailed"
 
     return logging_config
 
-
 logging.config.dictConfig(config=default_logging_config(level='DEBUG'))
 glue_job_logger: logging.Logger = logging.getLogger(name='job')
 
+def create_etl_log_table(n:int=0) -> None:
+    '''Creates the Athena _etl_log table'''
 
-def log(workflow_name: str, workflow_run_id: str, job_name: str, job_run_id: str, str_message: str) -> None:
-    '''Writes to the job log file'''
+    database_name: str = aws_athena_database
+    sql_query: str = f'''CREATE TABLE IF NOT EXISTS "{aws_athena_database}".{AWS_ATHENA_LOG_TABLE}
+                            WITH (table_type = 'ICEBERG',
+                                format = 'ORC', 
+                                write_compression ='ZSTD',
+                                location = 's3://stg-dlk-{ENVIRONMENT}-ds-{n}-raw/iceberg/_etl_log/', 
+                                is_external = false,
+                            --      partitioning = ARRAY['month(dt)'],
+                                vacuum_min_snapshots_to_keep = 10,
+                                vacuum_max_snapshot_age_seconds = 604800
+                            ) 
+                            AS 
+                            SELECT 
+                                to_unixtime(current_timestamp)*1000 AS double_epoch_ts_log
+                                ,'ds{n}-pipeline' AS str_glue_workflow_name
+                                ,'0' AS str_glue_workflow_runid
+                                ,'stg-dlk-{ENVIRONMENT}-ds{n}-job-source-to-raw' AS str_glue_job_name
+                                ,0 AS int_glue_job_runid
+                                ,'auditLog' AS str_glue_job_step
+                                ,CAST(123456789 AS BIGINT) AS bigint_rows_retrieved
+                                ,CAST('' AS VARCHAR(2000)) AS str_error_message
+                            WHERE 1=0;
+                        '''
+
+    get_query_execution: dict = wr.athena.start_query_execution(sql=sql_query,
+                                                                database=database_name,
+                                                                wait=True)
+
+    print(f'{get_query_execution["Status"]["State"]} in {get_query_execution["Statistics"]["TotalExecutionTimeInMillis"]}ms')
+
+
+def log(workflow_name: str = WORKFLOW_NAME, workflow_run_id: str = WORKFLOW_RUN_ID, job_name: str = JOB_NAME,
+        job_run_id: str = str(JOB_RUN_ID), str_message: str = '', bigint_rows_retrieved: int = 0,
+        float_latest_epoch: float = 0, str_error_message: str = '') -> None:
+    '''Writes to the job log file and to the Athena _etl_log table'''
 
     time.sleep(LOG_DELAY)
+    float_latest_epoch = int(1000*float(time.time()))
+
     glue_job_logger.info(msg=f'WORKFLOW_NAME.WORKFLOW_RUN_ID|JOB_NAME.JOB_RUN_ID: {workflow_name}.{workflow_run_id}|{job_name}.{job_run_id}|{str_message}')
+
+    database_name: str = aws_athena_database
+    sql_query: str = f'''INSERT INTO "{aws_athena_database}".{AWS_ATHENA_LOG_TABLE}(double_epoch_ts_log,str_glue_workflow_name,str_glue_workflow_runid,
+                            str_glue_job_name,int_glue_job_runid,str_glue_job_step,bigint_rows_retrieved,str_error_message) 
+                        VALUES(:float_latest_epoch;,:str_glue_workflow_name;,:str_glue_workflow_runid;,
+                            :str_glue_job_name;,:int_glue_job_runid;,:str_glue_job_step;,:bigint_rows_retrieved;,:str_error_message;)'''
+
+    get_query_execution: dict = wr.athena.start_query_execution(sql=sql_query,
+                                                                database=database_name,
+                                                                params={
+                                                                    "float_latest_epoch": float_latest_epoch,
+                                                                    "str_glue_workflow_name": f"'{workflow_name}'",
+                                                                    "str_glue_workflow_runid": f"'{workflow_run_id}'",
+                                                                    "str_glue_job_name": f"'{job_name}'",
+                                                                    "int_glue_job_runid": job_run_id,
+                                                                    "str_glue_job_step": f"'{str_message}'",
+                                                                    "bigint_rows_retrieved": bigint_rows_retrieved,
+                                                                    "str_error_message": f"'{str_error_message}'"
+                                                                },
+                                                                wait=True)
+
+    print(f'{get_query_execution["Status"]["State"]} in {get_query_execution["Statistics"]["TotalExecutionTimeInMillis"]}ms')
+
+
+def get_last_loaded_epoch() -> float:
+    '''Returns the last loaded epoch from the Athena _etl_log table'''
+
+    database_name: str = AWS_ATHENA_DATABASE
+    sql_query: str = f'''SELECT MAX(double_epoch_ts_log) AS last_loaded_epoch FROM {AWS_ATHENA_LOG_TABLE}'''
+
+    df = wr.athena.read_sql_query(sql=sql_query, database=database_name, ctas_approach=False, s3_output=aws_athena_output)
+
+    # print(df)
+    return float(df['last_loaded_epoch'][0])
 
 
 @lru_cache(maxsize=None)
@@ -113,42 +208,33 @@ def fibonacci(n: int) -> int:
     return fib[n]
 
 
-def process_arguments(options) -> Dict[str, str]:
-    '''These parameters will be populated by the trigger job paramters in Workflow or default to the Job configuration parameters'''
-
-    args: dict[str, str] = getResolvedOptions(args=sys.argv, options=options)
-    return args
-
-
-def execute_s3_sql_files_athena(workflow_name: str, workflow_run_id: str, job_name: str, job_run_id: str):
+def execute_s3_sql_files_athena(workflow_name: str = WORKFLOW_NAME, workflow_run_id: str = WORKFLOW_RUN_ID,
+                                job_name: str = JOB_NAME, job_run_id: str = str(JOB_RUN_ID)):
     '''Reads files from S3'''
 
-    # if environment == 'vscode':
-    #     vscode_session = boto3.Session(
-    #         region_name=AWS_REGION, profile_name=AWS_PROFILE)
-    #     s3_client = vscode_session.client(service_name='s3')
-    # else:
     glue_session = boto3.Session()
     s3_client = glue_session.client('s3')
 
     s3 = boto3.resource('s3')
-    s3_bucket = s3.Bucket(SQL_QUERIES_BUCKET)
+    s3_bucket = s3.Bucket(sql_queries_bucket)
 
-    # Logs SQL_QUERIES_BUCKET.SQL_QUERIES_FOLDER
+    # Logs sql_queries_bucket.SQL_QUERIES_FOLDER
     log(workflow_name=workflow_name, workflow_run_id=workflow_run_id, job_name=job_name,
-            job_run_id=str(job_run_id), str_message=f'SQL_QUERIES_BUCKET/SQL_QUERIES_FOLDER: {SQL_QUERIES_BUCKET}/{SQL_QUERIES_FOLDER}')
+        job_run_id=str(job_run_id), str_message=f'SQL_QUERIES_BUCKET/SQL_QUERIES_FOLDER: {sql_queries_bucket}/{SQL_QUERIES_FOLDER}')
 
     for sqlfile in s3_bucket.objects.filter(Prefix=SQL_QUERIES_FOLDER):
         if sqlfile.key.endswith('sql'):
-            s3_file = s3_client.get_object(Bucket=SQL_QUERIES_BUCKET, Key=sqlfile.key)
-            querystring = (s3_file['Body'].read().decode('utf-8'))
+            s3_file = s3_client.get_object(Bucket=sql_queries_bucket, Key=sqlfile.key)
+            querystring = (s3_file['Body'].read().decode('utf-8')).replace('${var.env}',ENVIRONMENT)
+            print(querystring)
 
             log(workflow_name=workflow_name, workflow_run_id=workflow_run_id, job_name=job_name,
-                job_run_id=str(job_run_id), str_message=f'Running Query:\n{querystring}')
-            df = wr.athena.read_sql_query(sql=querystring, database=AWS_ATHENA_DATABASE, ctas_approach=False, s3_output=AWS_ATHENA_OUPUT)
+                job_run_id=str(job_run_id), str_message=querystring.partition('\n')[0])
+            
+            df = wr.athena.read_sql_query(sql=querystring, database=aws_athena_database, ctas_approach=False, s3_output=aws_athena_output)
 
             log(workflow_name=workflow_name, workflow_run_id=workflow_run_id, job_name=job_name,
-                job_run_id=str(job_run_id), str_message=f'Filename: {sqlfile.key} - Returned rows: {df.shape[0]:4}')
+                job_run_id=str(job_run_id), str_message=f'Filename: {sqlfile.key}', bigint_rows_retrieved=df.shape[0])
 
             if df.shape[0] > 0:
                 log(workflow_name=workflow_name, workflow_run_id=workflow_run_id, job_name=job_name,
@@ -158,27 +244,23 @@ def execute_s3_sql_files_athena(workflow_name: str, workflow_run_id: str, job_na
 def log_environment():
     '''Logs variables for the job'''
 
-    try:
-        env_dict = {}
-        env_dict["Platform"] = platform.platform()
-        env_dict["Python"] = platform.python_version()
-        env_dict["boto3"] = boto3.__version__
-        env_dict["awswrangler"] = wr.__version__
-        env_dict["AWS_REGION"] = AWS_REGION
-        env_dict["AWS_BUCKET"] = AWS_BUCKET
-        env_dict["AWS_ATHENA_DATABASE"] = AWS_ATHENA_DATABASE
-        env_dict["S3_BUCKET"] = S3_DESTINATION_BUCKET
-        env_dict["S3_PATH"] = S3_DESTINATION_PATH
+    env_dict = {}
+    env_dict["Platform"] = platform.platform()
+    env_dict["Python"] = platform.python_version()
+    # env_dict["PYTHONPATH"]              = sys.path
+    env_dict["boto3"] = boto3.__version__
+    env_dict["awswrangler"] = wr.__version__
+    env_dict["AWS_REGION"] = AWS_REGION
+    env_dict["AWS_BUCKET"] = aws_bucket
+    env_dict["AWS_ATHENA_DATABASE"] = aws_athena_database
+    env_dict["S3_BUCKET"] = s3_bucket
+    env_dict["S3_PATH"] = s3_path
 
-        time.sleep(LOG_DELAY)
-        print(tabulate(env_dict.items(), headers=['variable', 'value'], tablefmt='psql', showindex=False))
-        time.sleep(LOG_DELAY)
-        print(f'PYTHONPATH: {sys.path}')
-    except Exception as exc:
-        # Log the exception
-        # log(workflow_name=workflow_name, workflow_run_id=workflow_run_id, job_name=job_name,
-        #    job_run_id=job_run_id, str_message=f'An error occurred:{str(exc)}')
-        raise
+    time.sleep(LOG_DELAY)
+    print(tabulate(env_dict.items(), headers=['variable', 'value'], tablefmt='psql', showindex=False))
+    time.sleep(LOG_DELAY)
+    print(f'PYTHONPATH: {sys.path}')
+    return JOB_RUN_ID, ENVIRONMENT
 
 
 def get_args(args):
@@ -191,18 +273,14 @@ def get_args(args):
     return env_value
 
 
-def send_sns(workflow_name: str, workflow_run_id: str, job_name: str, job_run_id: str, exc: Any) -> None:
+def send_sns(workflow_name: str = WORKFLOW_NAME, workflow_run_id: str = WORKFLOW_RUN_ID,
+             job_name: str = JOB_NAME, job_run_id: str = str(JOB_RUN_ID), exc: Any = '') -> None:
     '''Sends an SNS message to the failure topic'''
 
-    try:
-        sns = boto3.client('sns')
-        response = sns.publish(TopicArn=SNS_FAILURE_TOPIC,
-                               Message=f'WORKFLOW_NAME={workflow_name}\nWORKFLOW_RUN_ID={workflow_run_id}\nJOB_NAME={job_name}\nJOB_RUN_ID={job_run_id}\n{str(exc)}')
-    except Exception as exc:
-        # Log the exception
-        # log(workflow_name=workflow_name, workflow_run_id=workflow_run_id, job_name=job_name,
-        #    job_run_id=job_run_id, str_message=f'An error occurred:{str(exc)}')
-        raise
+    sns = boto3.client('sns')
+    response = sns.publish(TopicArn=SNS_FAILURE_TOPIC,
+                           Message=f'WORKFLOW_NAME={workflow_name}\nWORKFLOW_RUN_ID={workflow_run_id}\nJOB_NAME={job_name}\nJOB_RUN_ID={job_run_id}\n{str(exc)}')
+    print(response)
 
 
 if __name__ == '__main__':
--- ds14-pipeline/artifacts/ds14/glue/code/stg-dlk-sbx-ds14-job-source-to-raw.py
+++ ds14-pipeline/artifacts/ds14/glue/code/stg-dlk-sbx-ds14-job-source-to-raw.py
@@ -3,7 +3,8 @@
 # Python Shell jobs on AWS Glue:
 # 1. Gets the latest JSON files from the bucket stg-dlk-sbx-ds-14-raw, folder events_feed/events_jsonl/
 # 2. Downloads JSON file, extracts JSON array and recompress it (1DPU?)
-# 3. Uploads to stg-dlk-sbx-ds-14-raw S3 bucket for further processing (deletes original file?)
+# 3. Uploads to stg-dlk-sbx-ds-14-raw S3 bucket for further processing
+# 4. Moves the original file to the folder events_feed/events_jsonl/processed
 # davide.moraschi@toptal.com 2024
 '''
 
@@ -13,38 +14,28 @@ import sys
 import time
 import boto3
 from tabulate import tabulate
-from common_functions import log, log_environment, process_arguments, send_sns
+from common_functions import log, log_environment, process_arguments, send_sns, create_etl_log_table
 from config import AWS_BUCKET, AWS_FOLDER, S3_DESTINATION_BUCKET, S3_DESTINATION_PATH, S3_PROCESSED_FOLDER
 from gzip_s3_and_json_py3 import upload_json_gz
 from typing import Dict, List
 from typing import Any
 
-# Process command line arguments
-args: Dict[str, str] = process_arguments(options=['WORKFLOW_NAME', 'WORKFLOW_RUN_ID', 'EVENT_TYPES'])
 
-# Print arguments for debugging
-print(tabulate(tabular_data=args.items(), headers=['args.keys()', 'args.values()'], tablefmt='psql', showindex=False))
-
-# Parse report IDs, workflow name, EVENT_TYPES, and job name from arguments
-EVENT_TYPES: List[str] = json.loads(args['EVENT_TYPES'])
-WORKFLOW_NAME: str = args['WORKFLOW_NAME']
-WORKFLOW_RUN_ID: str = args['WORKFLOW_RUN_ID']
-JOB_NAME: str = args['JOB_NAME'] if 'JOB_NAME' in args else 'stg-dlk-sbx-ds14-job-source-to-raw'
-JOB_RUN_ID: int = int(time.time())
+events: Dict[str, str] = process_arguments(options=['EVENT_TYPES'])
+EVENT_TYPES: List[str] = json.loads(events['EVENT_TYPES'])
 
 
-def move_to_processed_folder(s3_client: Any, from_file_key: Any, to_file_key: Any) -> None:
+def move_to_processed_folder(s3_client: Any, aws_bucket: str, from_file_key: Any, to_file_key: Any) -> None:
     '''Moves a file from one source folder to a destination one'''
 
     # Copy the file to the destination folder
     try:
-        s3_client.copy_object(Bucket=AWS_BUCKET, CopySource=AWS_BUCKET + '/' + from_file_key.key, Key=S3_PROCESSED_FOLDER + to_file_key)
+        s3_client.copy_object(Bucket=aws_bucket, CopySource=aws_bucket + '/' + from_file_key.key, Key=S3_PROCESSED_FOLDER + to_file_key)
         # Delete the original file
-        s3_client.delete_object(Bucket=AWS_BUCKET, Key=from_file_key.key)
+        s3_client.delete_object(Bucket=aws_bucket, Key=from_file_key.key)
     except Exception as exc:
         # Log the exception
-        log(workflow_name=WORKFLOW_NAME, workflow_run_id=WORKFLOW_RUN_ID, job_name=JOB_NAME,
-            job_run_id=str(JOB_RUN_ID), str_message=f'An error occurred:{str(exc)}')
+        log(str_message='ERROR', bigint_rows_retrieved=-1, str_error_message=f'str({exc})')
         raise
 
 
@@ -53,11 +44,16 @@ def main() -> None:
 
     try:
         # Log environment information for debugging
-        log_environment()
+        job_run_id, environment = log_environment()
+        
+        # Creates the log table if not exists
+        create_etl_log_table(14)
 
         # Log start of job
-        log(workflow_name=WORKFLOW_NAME, workflow_run_id=WORKFLOW_RUN_ID, job_name=JOB_NAME,
-            job_run_id=str(JOB_RUN_ID), str_message='start-job')
+        log(str_message='start-job')
+
+        # Generate bucket name
+        aws_bucket = AWS_BUCKET.replace('${var.env}',environment)
 
         # Creates boto3 session/client
         glue_job_session = boto3.Session()
@@ -65,7 +61,7 @@ def main() -> None:
 
         # Creates a S3 resource and gets the bucket
         s3_resource = boto3.resource(service_name='s3')
-        json_bucket = s3_resource.Bucket(name=AWS_BUCKET)
+        json_bucket = s3_resource.Bucket(name=aws_bucket)
         total_file_count = 0
 
         # Loops for every JSON file in the ext bucket/folder
@@ -76,41 +72,38 @@ def main() -> None:
                 # Gets the event type from the file name
                 event_type = json_file.key.split(AWS_FOLDER)[1].split('/')[1].split('.')[1]
                 if event_type in EVENT_TYPES:
+
+                    # Log current file
+                    log(str_message=f'Processing file:{json_file.key}')
                     total_file_count +=1
                     
                     # Reads the file
-                    s3_file = s3_client.get_object(Bucket=AWS_BUCKET, Key=json_file.key)
+                    s3_file = s3_client.get_object(Bucket=aws_bucket, Key=json_file.key)
                     json_payload = s3_file['Body'].read().decode('utf-8')
 
                     # Log file upload
                     s3_filename: str = json_file.key.replace(AWS_FOLDER,'/') # json_file.key.split(AWS_FOLDER)[1].split('/') #[1]
-                    # print(s3_filename)
-                    log(workflow_name=WORKFLOW_NAME, workflow_run_id=WORKFLOW_RUN_ID, job_name=JOB_NAME,
-                        job_run_id=str(JOB_RUN_ID), str_message=f'Uploading file: {s3_filename}')
+                    log(str_message=f'Uploading file: {s3_filename}')
 
                     # Uploads to S3 raw bucket/folder
-                    upload_json_gz(s3client=s3_client, bucket=S3_DESTINATION_BUCKET,
+                    upload_json_gz(s3client=s3_client, bucket=S3_DESTINATION_BUCKET.replace('${var.env}',environment),
                                    key=S3_DESTINATION_PATH + event_type + '{0}.gz'.format(s3_filename), obj=json_payload)
 
                     # Moves it from the ext folder to the processed one
-                    move_to_processed_folder(s3_client=s3_client, from_file_key=json_file, to_file_key=s3_filename)
+                    move_to_processed_folder(s3_client=s3_client,aws_bucket=aws_bucket, from_file_key=json_file, to_file_key=s3_filename)
 
-        # Log end of job
-        log(workflow_name=WORKFLOW_NAME, workflow_run_id=WORKFLOW_RUN_ID, job_name=JOB_NAME,
-            job_run_id=str(JOB_RUN_ID), str_message='end-job')
+        # Log succesful end of job
+        log(str_message='end-job', bigint_rows_retrieved=total_file_count, str_error_message=f'Success.')
 
     except Exception as exc:
         # Log error
-        log(workflow_name=WORKFLOW_NAME, workflow_run_id=WORKFLOW_RUN_ID, job_name=JOB_NAME,
-            job_run_id=str(JOB_RUN_ID), str_message=f'An error occurred:{str(exc)}')
+        log(str_message='ERROR', bigint_rows_retrieved=-1, str_error_message=f'str({exc})')
 
         # Send SNS notification of error
-        send_sns(workflow_name=WORKFLOW_NAME, workflow_run_id=WORKFLOW_RUN_ID, job_name=JOB_NAME,
-                 job_run_id=str(JOB_RUN_ID), exc=exc)
+        send_sns(exc=exc)
 
-        # Log end of job
-        log(workflow_name=WORKFLOW_NAME, workflow_run_id=WORKFLOW_RUN_ID, job_name=JOB_NAME,
-            job_run_id=str(JOB_RUN_ID), str_message='end-job')
+        # Log failed end of job
+        log(str_message='end-job', str_error_message=f'Failure.')
 
         # Reraise exception
         raise
